97a85ddb275231081405f31b801b44ef2a3e5f44
cpeter <827963585@qq.com>
2019-04-03 17:40:57 +0800

    优化代码

--- src/main/java/com/soso/sosospider/service/AsyncService.java
+++ src/main/java/com/soso/sosospider/service/AsyncService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class AsyncService {
