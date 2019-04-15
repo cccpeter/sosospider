0c5319b4aaa143db6d86f5e0a08a2391c0b1ddde
cpeter <827963585@qq.com>
2019-04-16 01:30:31 +0800

    前端接入爬虫节点数据

--- php-api/application/index/controller/Index.php
+++ php-api/application/index/controller/Index.php
@@ -72,28 +72,10 @@ class Index extends Base
     	$re=json_encode(['data'=>$data,'day'=>$day,'code'=>'200']);
     	return $re;
     }
-  //   public function search(){
-  //   	$keyword=input('post.key');
-  //   	$elasticsearch = new ElasticsearchService ();
-		// $query=stripslashes('{"query":{"multi_match":{"query":"keyword","fields":"content"}},"from":0,"size":10,"highlight":{"fields":{"title":{},"content":{}}}}');
-		// $query=str_replace("keyword",$keyword,$query);
-		// $query=json_decode($query);
-		// $result= $elasticsearch->search ( $query );
-		// $re=json_decode($result,TRUE);
-		// 		for($i=0;$i<sizeof($re["hits"]["hits"]);$i++){
-		// 	$searchresult[$i]['content']=$re["hits"]["hits"][$i]["highlight"]["content"];
-		// 	$contextall="";
-		// 	for($j=0;$j<sizeof($searchresult[$i]['content']);$j++){
-		// 		$contextall.=$searchresult[$i]['content'][$j];
-		// 	}
-		// 	$searchresult[$i]['content']=mb_substr($contextall,20,600,"utf-8");
-		// 	$searchresult[$i]['content'].="……";
-		// 	$searchresult[$i]['url']=$re['hits']['hits'][$i]["_source"]["url"];
-		// 	$searchresult[$i]['title']=mb_substr($re['hits']['hits'][$i]["_source"]["title"],0,200,"utf-8");
-		// 	$searchresult[$i]['time']=$re['hits']['hits'][$i]["_source"]["time"];
-		// 	$searchresult[$i]['grade']=$re['hits']['hits'][$i]["_score"];
-		// }	
-		// dump($searchresult);
 
-  //   }
+    public function systemlist(){
+    	$spider_list=db("spider")->select();
+    	$data=json_encode(['data'=>$spider_list]);
+    	return $data;
+    }
 }
